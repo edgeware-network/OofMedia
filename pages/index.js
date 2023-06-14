51645ae5334419127b5e68b29f429d24427e9ed7
pranavcpawar <pranavcpawar@outlook.com>
2023-06-14 23:24:11 +0530

    create next app with tailwindcss

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { ConnectWallet } from "@thirdweb-dev/react";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <ConnectWallet theme="dark"/>
+      </main>
+    </div>
+  );
+}
