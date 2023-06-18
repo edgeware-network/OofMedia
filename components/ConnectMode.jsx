ba2a2b40d13f298c9533d9b1db3b5cf180427970
Raj Raorane <41839716+Raj-RR1@users.noreply.github.com>
2023-06-18 15:56:01 +0530

    filters

--- components/ConnectMode.jsx
+++ components/ConnectMode.jsx
@@ -2,11 +2,16 @@ import React from "react"
 
 const ConnectMode = () => {
     const [isVideoSelected, setIsVideoSelected] = React.useState(true);
-    const options = ["Gender", "SFW"];
+    const [isGenderMale, setisGenderMale] = React.useState(true);
+
 
     const handleToggle = () => {
         setIsVideoSelected(!isVideoSelected);
     }
+    const handleGenderSelected= () => {
+        setisGenderMale(!isGenderMale);
+    }
+
     return (
         <div className="flex outline outline-1 w-full justify-center bg-[#0d7377] rounded-lg p-4">
             <form className="mt-2 mb-2">
@@ -25,12 +30,33 @@ const ConnectMode = () => {
                 </div>
                 <div className="mt-4">
                     <label htmlFor="keywords" className="text-lg  mr-2">Keywords:</label>
-                    <input type="text" id="keywords" placeholder="Enter Keywords"  style={{fontFamily:"Poppins,sans-serif"}} className="mt-1 bg-gray-300 text-sm p-2 text-zinc-900 rounded-md shadow-sm 
+                    <input type="text" id="keywords" placeholder="Enter Keywords" style={{ fontFamily: "Poppins,sans-serif" }} className="mt-1 bg-gray-300 text-sm p-2 text-zinc-900 rounded-md shadow-sm 
                 focus:ring-indigo-500 focus:border-indigo-500"></input>
                 </div>
-                <div className="mt-4  flex">
-                <label htmlFor="keywords" className="text-lg  mr-2">Filters:</label>
-                <select id="filter" multiple className="mt-1 w-full bg-gray-300 rounded-md text-sm shadow-sm text-zinc-900">{options.map((option)=>(<option key={option}>{option}</option>))}</select>
+                <div className="mt-4 ">
+                    <label htmlFor="filter" className="text-lg">Filters: </label>
+                    <div className="mt-1 flex">
+                        <label htmlFor="filter" className="text-lg mr-8">Gender:</label>
+                        <select className="select select-bordered w-40 h-8">
+                            <option disabled selected>Select Gender</option>
+                            <option>Male</option>
+                            <option>Female</option>
+                        </select>
+                    </div>
+                    <div className="flex items-center mt-4">
+                    <label htmlFor="toggle2" className="block mr-8 text-lg ">SFW: </label>
+
+                    <span className="text-sm mr-2">Male</span>
+                    <label htmlFor="toggle2" className="cursor-pointer">
+                        <div className="relative">
+                            <input type="checkbox" id="toggle2" className="sr-only" checked={isGenderMale} onChange={handleGenderSelected}></input>
+                            <div className="block bg-gray-600 w-14 h-8 rounded-full"></div>
+                            <div className={`dot absolute ${isGenderMale ? "left-7" : "left-1"} top-1 bg-white w-6 h-6 rounded-full transition`}></div>
+                        </div>
+                    </label>
+                    <span className="text-sm ml-2">Female</span>
+                </div>
+
                 </div>
                 <div className="mt-4  ml-8 flex justify-center">
                     <button type="submit" className="py-2 w-20 ext-sm bg-indigo-600 rounded-md hover:bg-indigo-700">Match</button>
